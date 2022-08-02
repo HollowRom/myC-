0a002d962b702c28c3b6b5bca092042b950270b6
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-08-02 16:09:00 +0800

    update

--- ClassLibrary1/ClassLibrary1/生产订单取源单填写批号插件.cs
+++ ClassLibrary1/ClassLibrary1/生产订单取源单填写批号插件.cs
@@ -6,36 +6,37 @@ using Kingdee.BOS.Orm.DataEntity;
 
 namespace Kingdee.Bos.ProJect.BillBeforeSave.Plugln
 {
-    [Description("生产订单取源单填写批号插件")]
+    [Description("生产订单取源单填写批号插件2")]
     [Kingdee.BOS.Util.HotUpdate]
-    public class workOrderWriteLot : AbstractBillPlugIn
+    public class workOrderWriteLot2 : AbstractBillPlugIn
     {
         public override void BarItemClick(BarItemClickEventArgs e)
         {
             base.BarItemClick(e);
+
+            //this.View.ShowMessage("214141");
             if (e.BarItemKey.Equals("tbSplitSave"))
             {
-                //this.View.ShowMessage(this.View.Model.GetEntryRowCount("FTreeEntity").ToString());
-
-                string oldLot;
+                //return;
+                //string oldLot;
                 int rowNum = this.View.Model.GetEntryRowCount("FTreeEntity");
 
                 for (int idx = 0; idx < rowNum; idx++)
                 {
-                    
+
                     if (!this.View.GetFieldEditor("FLot", idx).Enabled)
                     {
                         continue;
                     }
 
-                    oldLot = "";
+                    //oldLot = "";
 
-                    oldLot = this.View.Model.GetValue("FLot", idx).ToString();
+                    //oldLot = this.View.Model.GetValue("FLot", idx).ToString();
 
-                    if (oldLot == "")
-                    {
-                        continue;
-                    }
+                    //if (oldLot == "")
+                    //{
+                    //    continue;
+                    //}
 
                     long sourceId = (long)this.View.Model.GetValue("FSALEORDERID", idx);
 
