005c35d36f4e06d5ab9209748166884dca32c48f
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-08-14 10:12:15 +0800

    update

---
+++ xkSDK/WorkflowScripts/init.py
@@ -0,0 +1,21 @@
+﻿from System import DateTime
+import clr
+clr.AddReference("Kingdee.BOS.Workflow")
+from Kingdee.BOS.Workflow import *
+
+#日期转换函数
+def date(str):
+	return DateTime.Parse(str)
+
+#字符串比较，忽略大小写
+def StringEquals(strA, strB):
+    return CommonFunction.StringEquals(strA, strB)
+
+#获取单据值
+def GetBillFieldValue(pkValue, formId, key):
+    return CommonFunction.GetBosVariableValue(ctx, pkValue, formId, key)
+
+#按默单据变量和单据主键变量或取字段名
+def GetBillFieldValue(key):
+    return CommonFunction.GetBosVariableValue(ctx, key)
+
