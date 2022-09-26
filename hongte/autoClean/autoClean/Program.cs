6d1a217a7d40584a99f8a31a073426d84d40a47e
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-09-26 19:36:08 +0800

    update

---
+++ hongte/autoClean/autoClean/Program.cs
@@ -0,0 +1,45 @@
+﻿using Kingdee.BOS.WebApi.Client;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+
+namespace autoClean
+{
+    class autoClean
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://127.0.0.1/k3cloud/";
+            const string dbid = "5f3a43d65be160";
+            const string userName = "administrator";
+            const string password = "888888";
+            const int lcid = 2052;
+            const int sleepTime = 300;
+            while (true)
+            {
+                var apiClient = new K3CloudApiClient(url);
+                var isLoginOk = apiClient.Login(dbid, userName, password, lcid);
+                var rval = false; // Action
+                if (isLoginOk)
+                {
+                    var times = 100;
+                    while (times > 0)
+                    {
+                        times--;
+                        apiClient.Execute<bool>(
+                            "Jac.XkDemo.BOS.WebApi.CacheManagerWebApiService.ClearCacheByFormIds,Jac.XkDemo.BOS.WebApi",
+                            new object[] { new List<string>(new[] { "BD_MATERIAL" }) });
+                        Console.WriteLine("执行了一次清理:" + DateTime.Now.ToLocalTime());  
+                        Thread.Sleep(1000 * sleepTime);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("登录失败:" + DateTime.Now.ToLocalTime());  
+                    Thread.Sleep(1000 * sleepTime);;
+                }
+            }
+        }
+    }
+}
+
