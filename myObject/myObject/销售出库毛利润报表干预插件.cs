c2d9cc5ae5d2c30a853415c78d57402d2657a77f
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-09-30 13:06:00 +0800

    update

--- myObject/myObject/销售出库毛利润报表干预插件.cs
+++ myObject/myObject/销售出库毛利润报表干预插件.cs
@@ -36,40 +36,49 @@ namespace myObject
               
         private void AddMnemonicCode(IRptParams filter, string tableName)
         {
-            var fieldListCustomExtension = new List<string>() { "", "" };
+            
+            var fieldListCustomExtension = new List<string>() { "F_VBDA_Text" };
             var fieldListOriginal = new List<string>();
             var strFilter = "";
-            if (CheckIsNeedChange(filter, fieldListCustomExtension, fieldListOriginal))
-            {
-                strFilter = filter.FilterParameter.FilterString;
-                filter.FilterParameter.FilterString = " 2 > 1 ";
-            }
-            else
-            {
-                base.BuilderReportSqlAndTempTable(filter, tableName);
-                return;
-            }
+            //if (CheckIsNeedChange(filter, fieldListCustomExtension, fieldListOriginal))
+            //{
+                StringBuilder sqlStr = new StringBuilder();
+                sqlStr.AppendFormat("alter table {0} add F_VBDA_Text nvarchar(255) ", tableName);
+                DBUtils.Execute(this.Context, sqlStr.ToString());
 
-            for (int i = 0; i < fieldListCustomExtension.Count; i++)
-            {
-                _ = strFilter.Replace(fieldListCustomExtension[i], "T0." + fieldListCustomExtension[i]);
-            }
+                sqlStr.Clear();
+                sqlStr.AppendFormat(" MERGE INTO {0} T0 ", tableName);
+                sqlStr.AppendFormat(" using test_view as T99");
+                sqlStr.AppendFormat(" on T0.FBILLNO = T99.FBILLNO");
+                sqlStr.AppendFormat(" when matched then update set F_VBDA_Text = T99.fid ");
+                DBUtils.Execute(this.Context, sqlStr.ToString());
 
-            for (int i = 0; i < fieldListOriginal.Count; i++)
-            {
-                _ = strFilter.Replace(fieldListOriginal[i], "T99." + fieldListOriginal[i]);
-            }
+                sqlStr.Clear();
+                strFilter = "";
+                //strFilter = filter.FilterParameter.FilterString;
+                //filter.FilterParameter.FilterString = " 2 > 1 ";
+
+                //for (int i = 0; i < fieldListCustomExtension.Count; i++)
+                //{
+                //    _ = strFilter.Replace(fieldListCustomExtension[i], "T0." + fieldListCustomExtension[i]);
+                //}
+
+                //for (int i = 0; i < fieldListOriginal.Count; i++)
+                //{
+                //    _ = strFilter.Replace(fieldListOriginal[i], "T99." + fieldListOriginal[i]);
+                //}
+
+                sqlStr.AppendFormat(" delete {0} where not ( 1 = 1 {1})", tableName, strFilter);
+                DBUtils.Execute(this.Context, sqlStr.ToString());
+
             base.BuilderReportSqlAndTempTable(filter, tableName);
-            StringBuilder sqlStr = new StringBuilder();
-            sqlStr.AppendFormat("alter table {0} add F_VBDA_Text nvarchar(255) ", tableName);
-            DBUtils.Execute(this.Context, sqlStr.ToString());
+            //}
+            //else
+            //{
+            //    base.BuilderReportSqlAndTempTable(filter, tableName);
+            //    return;
+            //}
 
-            sqlStr.Clear();
-            sqlStr.AppendFormat(" MERGE INTO {0} T0 ", tableName);
-            sqlStr.AppendFormat(" using test_view as T99");
-            sqlStr.AppendFormat(" on T0.FBILLNO = T99.FBILLNO and {0}", strFilter);
-            sqlStr.AppendFormat(" when matched then update set F_VBDA_Text = T99.fid ");
-            DBUtils.Execute(this.Context, sqlStr.ToString());
         }
         
         public override ReportHeader GetReportHeaders(IRptParams filter)
