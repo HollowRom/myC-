b5be49ed8a2bcc50be1e8dd948d16a52cc3992a4
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-10-22 08:18:52 +0800

    update

---
+++ autoUpdPluin/autoUpdPluin/autoUpdPluin/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+
+namespace autoUpdPluin
+{
+    [Description("定时更新表头单据值")]
+    [Kingdee.BOS.Util.HotUpdate]
+    public class autoUpdLater : AbstractDynamicFormPlugIn
+    {
+        public override void OnTimerElapsed(EventArgs e)
+
+        {
+            base.OnTimerElapsed(e);
+
+            this.View.Model.SetValue("F_VBDA_Date99", DateTime.Now);
+
+        }
+    }
+}
---
+++ myObject/myObject/定时更新表头单据值.cs
@@ -0,0 +1,20 @@
+﻿using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using System;
+using System.ComponentModel;
+
+namespace myObject
+{
+    [Description("定时更新表头单据值")]
+    [Kingdee.BOS.Util.HotUpdate]
+    public class autoUpdLater : AbstractDynamicFormPlugIn
+    {
+        public override void OnTimerElapsed(EventArgs e)
+
+        {
+            base.OnTimerElapsed(e);
+
+            this.View.Model.SetValue("F_VBDA_Integer", Int32.Parse(this.View.Model.GetValue("F_VBDA_Integer").ToString()) + 1 % 10);
+
+        }
+    }
+}
