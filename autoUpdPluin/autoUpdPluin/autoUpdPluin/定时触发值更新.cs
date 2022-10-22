da39a231d1322bd0dfb122c15b59d8d075740b82
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-10-22 11:35:21 +0800

    update

---
+++ autoUpdPluin/autoUpdPluin/autoUpdPluin/定时触发值更新.cs
@@ -0,0 +1,20 @@
+﻿using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using System;
+using System.ComponentModel;
+
+namespace autoUpdPluin
+{
+    [Description("随心跳触发单据编号值更新事件")]
+    [Kingdee.BOS.Util.HotUpdate]
+    public class autoUpdField : AbstractDynamicFormPlugIn
+    {
+        const string fieldName = "FBILLNO";//可以考虑放在配置文件中,但是每次执行都会重新读取
+        public override void OnTimerElapsed(EventArgs e)
+
+        {
+            base.OnTimerElapsed(e);
+
+            this.View.InvokeFieldUpdateService(fieldName, 0);
+        }
+    }
+}
