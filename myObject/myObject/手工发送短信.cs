f5f7b55c62567d826fe27e32b82c85b2c1b19728
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-11-29 08:25:20 +0800

    update

--- myObject/myObject/手工发送短信.cs
+++ myObject/myObject/手工发送短信.cs
@@ -1,7 +1,11 @@
-﻿using Kingdee.BOS.BusinessEntity.CloudPlatform;
+﻿using Kingdee.BOS;
+using Kingdee.BOS.BusinessEntity.CloudPlatform;
+using Kingdee.BOS.Contracts;
+using Kingdee.BOS.Core;
 using Kingdee.BOS.Core.DynamicForm.PlugIn;
 using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
 using Kingdee.BOS.Msg;
+using Kingdee.BOS.ServiceHelper;
 using Kingdee.BOS.Util;
 using System;
 using System.Collections.Generic;
@@ -11,65 +15,23 @@ using System.Text;
 
 namespace myObject
 {
-    [Description("手工发送短信"), HotUpdate]
-    public class send_mess_not_auto: AbstractDynamicFormPlugIn
+    [Description("自动发送短信"), HotUpdate]
+    public class send_mess_auto: IScheduleService
     {
-        public override void BarItemClick(BarItemClickEventArgs e)
-
+        public void Run(Context ctx, Schedule schedule)
         {
-
-            base.BarItemClick(e);
-
-            if (e.BarItemKey.Equals("VBDA_tbButton", StringComparison.OrdinalIgnoreCase))
-
-            {
-
-                SendMessage();
-
-            }
-
-        }
-
-
-
-        private void SendMessage()
-
-        {
-
             var phoneinfolist = new List<PhoneInfo>();
-
             var phoneinfo = new PhoneInfo();
-
             // 接收短信的手机号码
-
-            phoneinfo.Phone = "18867710321";
-
+            phoneinfo.Phone = "15658510227";
             // 接收短信的用户（如果不是当前系统的用户，可不填）
-
-            phoneinfo.ReceiverUser = this.Context.UserId;
-
+            phoneinfo.ReceiverUser = ctx.UserId;
             phoneinfolist.Add(phoneinfo);
-
             var smsInfo = new CloudSMSInfo();
-
-
-
             // 短信内容
-
             smsInfo.SMSMessage = "老板,现在是法治社会,欠钱不能赖账";
-
             smsInfo.PhoneInfos = phoneinfolist;
-
-            var result = Kingdee.BOS.ServiceHelper.SMSServiceHelper.SendMessage(this.Context, smsInfo);
-
-            if (result.Successful)
-
-            {
-
-                this.View.ShowMessage("短信发送成功！");
-
-            }
-
+            var result = SMSServiceHelper.SendMessage(ctx, smsInfo);
         }
     }
 }
---
+++ myObject/myObject/自动发送短信.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace myObject
+{
+    public class 自动发送短信
+    {
+    }
+}
