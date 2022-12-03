0b4ff8bea02823874dd08f292ff01bd51aed9e88
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-12-03 14:39:45 +0800

    update

---
+++ myObject/myObject/委外订单解锁bom版本号.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.Util;
+using Kingdee.K3.MFG.SUB.Business.PlugIn.Bill;
+
+namespace myObject
+{
+    [Description("委外订单解锁bom版本号")]
+    [HotUpdate]
+    public class unlock_wwdd_bom_filed : SubReqOrderEdit
+    {
+        public override void EntryBarItemClick(BarItemClickEventArgs e)
+        {
+            base.EntryBarItemClick(e);
+            if (e.BarItemKey.ToUpperInvariant() == "VBDA_TBBUTTON")
+            {
+                DynamicObjectCollection entityDataObject = Model.GetEntityDataObject(View.Model.BusinessInfo.GetEntity("FTreeEntity"));
+
+                for (int idx = 0; idx < entityDataObject.Count; idx++)
+                {
+                    //this.View.GetFieldEditor("FMaterialId", idx).SetEnabled("", true);
+                    //this.View.GetFieldEditor("FBomId", idx).SetEnabled("", true);
+                    //this.View.GetFieldEditor("FUnitID", idx).SetEnabled("", true);
+                    //this.View.GetFieldEditor("F_VBDA_Base1", idx).SetEnabled("", true);
+
+                }
+                this.View.ShowMessage("aabb3");
+            }
+        }
+
+        public override void AfterBindData(EventArgs e)
+        {
+            //this.View.GetControl("FBomId").Enabled = true;
+            //this.View.StyleManager.SetEnabled("FBomId", "", true);
+            //this.View.StyleManager.SetEnabled("FBomId", "BillStatusByHead", true);
+
+
+            //this.View.GetControl("FMaterialId").Enabled = true;
+            //this.View.StyleManager.SetEnabled("FMaterialId", "", true);
+            //this.View.StyleManager.SetEnabled("FMaterialId", "BillStatusByHead", true);
+
+
+            //this.View.GetControl("FDate").Enabled = true;
+            //this.View.StyleManager.SetEnabled("FDate", "", true);
+            //this.View.StyleManager.SetEnabled("FDate", "BillStatusByHead", true);
+            
+            DynamicObjectCollection entityDataObject = Model.GetEntityDataObject(View.Model.BusinessInfo.GetEntity("FTreeEntity"));
+
+            for (int idx = 0; idx < entityDataObject.Count; idx++)
+            {
+                //this.View.GetFieldEditor("FMaterialId", idx).SetEnabled("", true);
+                //this.View.GetFieldEditor("FBomId", idx).SetEnabled("", true);
+                //this.View.GetFieldEditor("FUnitID", idx).SetEnabled("", true);
+                //this.View.GetFieldEditor("F_VBDA_Base1", idx).SetEnabled("", true);
+            }
+
+            
+            this.View.ShowMessage("aabb3");
+        }
+    }
+}
--- myObject/myObject/手工核销测试插件.cs
+++ myObject/myObject/手工核销测试插件.cs
@@ -19,28 +19,33 @@ namespace myObject
             switch (e.BarItemKey)
             {
                 case "VBDA_tbButton":
+                    string billnos = "表头条件:";
                     object headStr = this.View.Model.GetValue("F_VBDA_Text");
                     if (headStr == null || headStr.ToString().Equals(""))
                     {
-                        this.View.ShowMessage("未填写条件");
-                        return;
+                        billnos = billnos + "为空\r\n";
+                        //this.View.ShowMessage("未填写条件");
+                        //return;
+                    } else
+                    {
+                        billnos = billnos + this.View.Model.GetValue("F_VBDA_Text").ToString() + "\r\n";
                     }
-                    string billnos = "表头条件:" + this.View.Model.GetValue("F_VBDA_Text").ToString() + "\r\n";
                     
                     //应收
                     DynamicObjectCollection entityD = Model.GetEntityDataObject(View.Model.BusinessInfo.GetEntity("FDebitEntry"));
                     
                     for (int idx = 0; idx < entityD.Count; idx++)
                     {
+                        billnos = billnos + this.View.Model.GetValue("FSRCBILLNOD", idx) + "--" + this.View.Model.GetValue("F_VBDA_TEXT5", idx) + "--";
                         this.View.Model.SetValue("FTheIsMatchD", (object)true, idx);
                     }
-
+                    billnos = billnos + "\r\n";
                     //应付
                     DynamicObjectCollection entityC = Model.GetEntityDataObject(View.Model.BusinessInfo.GetEntity("FCreditEntry"));
                     
                     for (int idx = 0; idx < entityC.Count; idx++)
                     {
-                        billnos = billnos + this.View.Model.GetValue("FSRCBILLNOC", idx);
+                        billnos = billnos + this.View.Model.GetValue("FSRCBILLNOC", idx) + "--" + this.View.Model.GetValue("F_VBDA_TEXT5", idx) + "--";
                         this.View.Model.SetValue("FTheIsMatchC", (object)true, idx);
                     }
                     this.View.ShowMessage(billnos);
