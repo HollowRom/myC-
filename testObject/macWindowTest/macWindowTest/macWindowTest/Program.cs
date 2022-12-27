5f020ca3a20ca359de89106ea2b61c98c591829f
HollowRom <54339070+HollowRom@users.noreply.github.com>
2022-12-27 11:47:05 +0800

    update

--- testObject/macWindowTest/macWindowTest/macWindowTest/Program.cs
+++ testObject/macWindowTest/macWindowTest/macWindowTest/Program.cs
@@ -162,7 +162,7 @@ namespace macWindowTest
                 this.listView1.Items.Add(lvi);
             }
         }
- 
+
         private void button6_Click(object sender, EventArgs e)
         {
             //显示选中内容
@@ -184,5 +184,45 @@ namespace macWindowTest
                 MessageBox.Show("未选中任何项");
             }
         }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            //
+            // dataGridView
+            //
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(517, 337);
+            this.dataGridView.TabIndex = 9;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            //
+            // CalificarVendedor
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 380);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "CalificarVendedor";
+            this.Text = "Calificar al Vendedor";
+            this.Load += new System.EventHandler(this.CalificarVendedor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+        }
+
     }
 }
