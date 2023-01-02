92eda8ccf53e108a582e1f2c7eee422694cef8a4
HollowRom <54339070+HollowRom@users.noreply.github.com>
2023-01-02 15:00:30 +0800

    update

---
+++ afObejct/anfuObject/anfuObject/应付单解锁采购部门.cs
@@ -0,0 +1,17 @@
+﻿using Kingdee.BOS.Core.Bill.PlugIn;
+using Kingdee.BOS.Util;
+using System;
+using System.ComponentModel;
+
+namespace anfuObject
+{
+    [Description("生产订单加载后解锁业务日期字段"), HotUpdate]
+    public class unlock_ysd_cgbm_filed : AbstractBillPlugIn
+    {
+        public override void AfterBindData(EventArgs e)
+        {
+            base.AfterBindData(e);
+            this.View.StyleManager.SetEnabled("FPURCHASEDEPTID", "BillStatusByHead", true);
+        }
+    }
+}
---
+++ myObject/myObject/应付单解锁采购部门.cs
@@ -0,0 +1,49 @@
+﻿using Kingdee.BOS.Business.Bill.PlugIn;
+using Kingdee.BOS.Core.Bill.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Util;
+using Kingdee.K3.MFG.SUB.Business.PlugIn.Bill;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace myObject
+{
+    //[Description("应收单解锁采购部门")]
+    //[HotUpdate]
+    //public class unlock_ysd_cgbm_filed : AbstractBillPlugIn
+    //{
+    //    public override void BarItemClick(BarItemClickEventArgs e)
+    //    {
+    //        base.EntryBarItemClick(e);
+    //        if (e.BarItemKey.ToUpperInvariant() == "VBDA_TBBUTTON")
+    //        {
+    //            this.View.StyleManager.SetEnabled("FPURCHASEDEPTID", "BillStatusByHead", true);
+    //        }
+    //    }
+
+    //}
+
+    [Description("生产订单加载后解锁业务日期字段"), HotUpdate]
+    public class unlock_ysd_cgbm_filed : AbstractBillPlugIn
+    {
+        //public override void AfterBindData(EventArgs e)
+        //{
+        //    base.AfterBindData(e);
+        //    this.View.GetControl("FPURCHASEDEPTID").Enabled = true;
+        //    this.View.GetControl("FPURCHASEDEPTID").Enabled = true;
+        //    this.View.ShowMessage("aabb");
+        //}
+
+        public override void AfterBindData(EventArgs e)
+        {
+            base.AfterBindData(e);
+            this.View.StyleManager.SetEnabled("FPURCHASEDEPTID", "BillStatusByHead", true);
+            //this.View.GetControl("FPURCHASEDEPTID").Enabled = true;
+            //this.View.GetControl("FPURCHASEDEPTID").Enabled = true;
+            this.View.ShowMessage("aabb2");
+        }
+    }
+}
