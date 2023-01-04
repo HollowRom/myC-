cdb7961cf9229224f3a87f2badfe8e1ab1b2f20d
HollowRom <54339070+HollowRom@users.noreply.github.com>
2023-01-04 15:30:58 +0800

    update

--- myObject/myObject/下推委外退料单报错.cs
+++ myObject/myObject/下推委外退料单报错.cs
@@ -1,14 +1,38 @@
-﻿using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn.Args;
+﻿using System;
+using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn.Args;
+using Kingdee.BOS.Core.Metadata.Util;
+using Kingdee.K3.Core.MFG.EntityHelper;
 using Kingdee.K3.MFG.SUB.App.BillConvertServicePlugIn;
 
 namespace myObject
 {
-    public class test_some_err : PPBom2ReturnItem
+    // public class test_some_err : PPBom2ReturnItem
+    // {
+    //     public override void OnParseFilterOptions(ParseFilterOptionsEventArgs e)
+    //     {
+    //         base.OperateOption.SetVariableValue("supBackFlush", true);
+    //         base.OnParseFilterOptions(e);
+    //     }
+    // }
+
+    public class test_some_err : PPBOM2ReturnMtrlConvert
     {
-        public override void OnParseFilterOptions(ParseFilterOptionsEventArgs e)
+        protected override void OnPrepareProperty(ConvertContext e)
         {
-            base.OperateOption.SetVariableValue("supBackFlush", true);
-            base.OnParseFilterOptions(e);
+            try
+            {
+                var rebl = e.GetPropertyValue("supBackFlush");
+                if (rebl == null)
+                {
+                    e.SetPropertyValue("supBackFlush", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                e.SetPropertyValue("supBackFlush", true);
+            }
+
+            base.OnPrepareProperty(e);
         }
     }
 }
