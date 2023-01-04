c3e2ed4924fda1ba75815af0151253987ea2523d
HollowRom <54339070+HollowRom@users.noreply.github.com>
2023-01-04 14:59:25 +0800

    update

---
+++ myObject/myObject/下推委外退料单报错.cs
@@ -0,0 +1,14 @@
+﻿using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn.Args;
+using Kingdee.K3.MFG.SUB.App.BillConvertServicePlugIn;
+
+namespace myObject
+{
+    public class test_some_err : PPBom2ReturnItem
+    {
+        public override void OnParseFilterOptions(ParseFilterOptionsEventArgs e)
+        {
+            base.OperateOption.SetVariableValue("supBackFlush", true);
+            base.OnParseFilterOptions(e);
+        }
+    }
+}
